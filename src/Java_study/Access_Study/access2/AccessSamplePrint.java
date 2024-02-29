c92f32db9d3f67dde5556a579b33eab077742a26
SO HEE KIM <155497383+sohee99@users.noreply.github.com>
2024-02-29 17:54:17 +0900

    Update AccessSamplePrint.java

--- src/Java_study/Access_Study/access2/AccessSamplePrint.java
+++ src/Java_study/Access_Study/access2/AccessSamplePrint.java
@@ -2,12 +2,12 @@ package Java_study.Access_Study.access2;
 
 
 public class AccessSamplePrint {
-/*
+
     public static void main(String[] args) {
         AccessSample access = new AccessSample();
         System.out.println(access.number4);
         //패키지가 다르기 때문에 public 인 number4만 가능
     }
 
- */
+ 
 }
